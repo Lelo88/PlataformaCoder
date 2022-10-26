8ab9991592bbfd0e724bd053830f5ee65b6ad61f
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2022-10-26 13:41:28 -0300

    Hasta acá las modificaciones de la Clase 21

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,13 +1,16 @@
 
 from django.urls import path
-from AppCoder.views import curso, cursos, entregables, estudiantes, inicio, lista_curso, profesores
+from .views import curso, cursoFormulario, cursos, entregables, estudiantes, inicio, lista_curso, profesores, busqueda_camada, buscar
 
 urlpatterns = [
     path('agrega-curso/<nombre>/<camada>', curso),
+    path('', inicio),
     path('muestra-lista/', lista_curso),
     path('cursos/', cursos, name='Cursos'),
     path('profesores/', profesores,name='Profesores'),
     path('estudiantes/', estudiantes,name='Estudiantes'),
     path('entregables/', entregables,name='Entregables'),
-    path('inicio/', inicio, name='Inicio'),
+    path('cursoFormulario/',cursoFormulario, name='CursoFormulario'),
+    path('busqueda_camada/', busqueda_camada, name="busqueda_camada"),
+    path('buscar/', buscar, name="buscar"),
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from .models import Curso
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+
+from .models import Curso
+from .forms import CursoFormulario
 
 # Create your views here.
 def curso(request, nombre, camada):
@@ -33,3 +35,34 @@ def profesores(request):
 
 def entregables(request):
     return render(request,'entregables.html')
+
+def cursoFormulario(request):
+    
+    print('method:', request.method)
+    print('post: ', request.POST)
+
+    if request.method == 'POST':
+        mi_formulario = CursoFormulario(request.POST)
+        print(mi_formulario)
+        if mi_formulario.is_valid():
+            data = mi_formulario.cleaned_data
+            curso = Curso(nombre=data['curso'], camada=data['camada'])
+            curso.save()
+            return redirect('Cursos')
+            # return render(request, 'inicio.html')
+    else:
+        mi_formulario = CursoFormulario()
+    
+    return render(request, "cursoFormulario.html", {'mi_formulario': mi_formulario})
+
+def busqueda_camada(request):
+    
+    return render(request, 'busqueda_camada.html')
+
+def buscar(request):
+    
+    camada_buscada = request.GET['camada']
+    
+    curso = Curso.objects.get(camada = camada_buscada)
+    
+    return render(request, 'resultadoBusqueda.html', {'curso': curso, 'camada': camada_buscada})
